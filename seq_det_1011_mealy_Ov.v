f65496fa6eaf710c8f72c4df277f05130f3083ee
Pankaj Chaudhari <72489198+Pankaj6198@users.noreply.github.com>
2022-10-15 21:24:10 +0530

    Add files via upload

---
+++ seq_det_1011_mealy_Ov.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:00:28 10/15/2022 
+// Design Name: 
+// Module Name:    seq_det_1011_mealy_Ov 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module seq_det_1011_mealy_Ov(input clk, input x, output reg y, output reg [1:0]st);
+    reg [1:0]state;
+    reg [1:0]next_state;
+    parameter S0 = 0,S1 = 1, S2 = 2, S3 = 3;
+    initial begin
+        state <= S0;
+        st <= S0;
+    end
+
+    always@(posedge clk)
+    begin
+        state <= next_state;
+        st <= next_state;
+    end
+
+    always@(state or x)
+    begin
+        y = 0;
+        case(state)
+        S0: if(x)
+                begin
+                    next_state = S1;
+                end
+            else
+                next_state = S0;
+
+        S1: if(x)
+                next_state = S1;
+            else
+                next_state = S2;
+
+        S2: if(x)
+                next_state = S3;
+            else
+                next_state = S0;
+        S3: if(x)
+                begin
+                    next_state = S1; y =1;
+                end
+            else
+                next_state = S2;
+        default:
+                next_state = S0;
+    endcase
+end
+endmodule
+
---
+++ seq_det_1011_tb.v
@@ -0,0 +1,67 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:08:25 10/15/2022
+// Design Name:   seq_det_1011_mealy_Ov
+// Module Name:   E:/Pankaj_HDL_119/seq_det_1011_tb.v
+// Project Name:  Pankaj_HDL_119
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: seq_det_1011_mealy_Ov
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module seq_det_1011_tb;
+
+	// Inputs
+	reg clk;
+	reg x;
+
+	// Outputs
+	wire y;
+	wire [1:0] st;
+
+	// Instantiate the Unit Under Test (UUT)
+	seq_det_1011_mealy_Ov uut (
+		.clk(clk), 
+		.x(x), 
+		.y(y), 
+		.st(st)
+	);
+
+	always
+        begin
+            clk = ~clk; #5;
+        end
+	initial begin
+        clk=0;
+        x=0;
+        #15;
+        x=1; #10;
+        x = 0; #10;
+        x=1; #10;
+        x=1; #10;
+        x=0; #10;
+        x = 1; #10;
+        x=1; #10;
+        $finish;
+    end
+	 
+    initial begin
+        $monitor("clk=%d state = %2d t=%3d x=%d output=%d \n",clk ,st,$time,x,y);
+    end
+	
+      
+endmodule
+

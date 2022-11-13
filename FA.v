45195eb4302e1828568a946f58d097025a9facb3
Pankaj Chaudhari <72489198+Pankaj6198@users.noreply.github.com>
2022-11-13 15:01:40 +0530

    Add files via upload

---
+++ FA.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:22:02 11/13/2022 
+// Design Name: 
+// Module Name:    FA 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FA(sum,carry,a,b,c);
+
+   input wire a,b,c;
+   output wire sum,carry;
+
+assign sum=a^b^c;
+assign carry=(a&b)|(b&c)|(c&a);
+endmodule
---
+++ FA_tb.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   14:23:33 11/13/2022
+// Design Name:   FA
+// Module Name:   E:/HDL_Pankaj_119/FA_tb.v
+// Project Name:  HDL_Pankaj_119
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FA
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module FA_tb;
+
+	// Inputs
+	reg a;
+	reg b;
+	reg c;
+
+	// Outputs
+	wire sum;
+	wire carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	FA uut (
+		.sum(sum), 
+		.carry(carry), 
+		.a(a), 
+		.b(b), 
+		.c(c)
+	);
+
+	initial begin
+$monitor($time,"a=%b b=%b c=%b sum=%b carry=%b",a,b,c,sum,carry);
+	a=0;
+	b=0;
+	c=0;
+#10
+	a=0;
+	b=0;
+	c=1;
+#10
+	a=0;
+	b=1;
+	c=0;
+#10
+	a=0;
+	b=1;
+	c=1;
+#10
+	a=1;
+	b=0;
+	c=0;
+#10
+	a=1;
+	b=0;
+	c=1;
+#10
+	a=1;
+	b=1;
+	c=0;
+#10
+	a=1;
+	b=1;
+	c=1;
+end
+endmodule

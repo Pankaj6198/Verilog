6463a4e781c8f198714d4a527a8e0b938e4d81d9
Pankaj Chaudhari <72489198+Pankaj6198@users.noreply.github.com>
2022-11-15 19:57:13 +0530

    Add files via upload

---
+++ HA.v
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:49:06 11/15/2022 
+// Design Name: 
+// Module Name:    HA 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//Data Flow Code:
+module HA_DF(
+    input a,
+    input b,
+    output sum,
+    output carry
+    );
+	 assign sum = a^b;
+	 assign carry = a & b;
+
+
+endmodule
+
+//2) Gate Level Code
+module ha_GL(
+    input a,
+    input b,
+    output sum,
+    output carry
+    );
+	 xor u1(sum,a,b);
+	 and u2(carry,a,b);
+
+
+endmodule
+
+//3)Behavioral Code
+module HA_behav(
+    input a,
+    input b,
+    output sum,
+    output carry
+    );
+	 reg sum,carry;
+	 always @(a or b)
+	 begin
+	 if (a==0 && b==0)
+	 begin
+		sum = 0;
+		carry = 0;
+		end
+	 else if (a==0 && b==1)
+	 begin
+		sum = 1;
+		carry = 0;
+		end
+	else if (a==1 && b==0)
+	 begin
+		sum = 1;
+		carry = 0;
+		end
+	else
+	 begin
+		sum = 0;
+		carry = 1;
+		end
+	 end
+
+
+endmodule
---
+++ HA_tb.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:54:05 11/15/2022
+// Design Name:   HA_DF
+// Module Name:   E:/HDL_Pankaj_119/HA_tb.v
+// Project Name:  HDL_Pankaj_119
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: HA_DF
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module HA_tb;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire sum;
+	wire carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	HA uut (
+		.a(a), 
+		.b(b), 
+		.sum(sum), 
+		.carry(carry)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+		#100; 		// Wait 100 ns for global reset to finish
+        
+		// Add stimulus here
+		
+		a = 0;
+		b = 1;
+		#100;
+		
+		a = 1;
+		b = 0;
+		#100;
+		
+		a = 1;
+		b = 1;
+		#100;
+	end
+      
+endmodule
+
